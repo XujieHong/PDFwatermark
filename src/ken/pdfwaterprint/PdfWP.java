b73f333dedbf0de56d0f683ebfbbb93483397c41
Ken Hong <xujie.hong@tcl.com>
2018-02-02 17:12:54 +0800

    First commit

---
+++ src/ken/pdfwaterprint/PdfWP.java
@@ -0,0 +1,160 @@
+package ken.pdfwaterprint;
+
+import javax.swing.JFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.dnd.DnDConstants;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.dnd.DropTarget; 
+import java.awt.dnd.DropTargetDragEvent; 
+import java.awt.dnd.DropTargetDropEvent; 
+import java.awt.dnd.DropTargetEvent; 
+import java.awt.dnd.DropTargetListener; 
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.DataFlavor;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class PdfWP extends JFrame implements ActionListener, DropTargetListener {
+	private static final long serialVersionUID = -3597288512867691569L;
+	JTextField labfilepath;
+	JPanel panel;
+	JButton btn, btn2;
+	String filepath;
+	JLabel lbl;
+	DropTarget dropTarget;
+
+	public PdfWP() {
+		filepath = "";
+		labfilepath = new JTextField(30);
+		btn = new JButton("Select PDF File:");
+		btn2 = new JButton("Watermark it");
+		btn.addActionListener(this);
+		btn2.addActionListener(this);
+
+		panel = new JPanel();
+		panel.add(btn);
+		panel.add(labfilepath);
+		panel.add(btn2);
+		
+		lbl = new JLabel("...");
+		panel.add(lbl);
+		
+		getContentPane().add(panel);
+		setTitle("WIL PDF waterprint");
+		setLocation(400,200);
+		setSize(500, 150);
+		setVisible(true);
+	
+		dropTarget = new DropTarget(this.lbl, DnDConstants.ACTION_COPY_OR_MOVE,this);
+		
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource() == btn) {
+			JFileChooser chooser = new JFileChooser();
+			chooser.setMultiSelectionEnabled(false);
+			chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+			chooser.setDialogTitle("Select your PDF file");
+			int result = chooser.showOpenDialog(this);
+			if (result == JFileChooser.APPROVE_OPTION) {
+					File file = chooser.getSelectedFile();
+					filepath = file.getAbsolutePath();
+					labfilepath.setText( filepath);
+				}
+		}else if (e.getSource() == btn2) {
+			//File file = new File(DEST);
+	        //file.getParentFile().mkdirs();
+			String filepathDst;
+			filepathDst = filepath.substring(0, filepath.length() - 4);
+			try{
+				new TransparentWatermark().manipulatePdf(filepath, filepathDst + "_wp.pdf");
+				//label.setText("Done!");
+				lbl.setText("Done!");
+			}catch (Exception ex){
+				
+			}
+		}
+	}
+
+
+	public static void main(String args[]) {
+		new PdfWP();
+	}
+
+	@Override
+	public void dragEnter(DropTargetDragEvent dtde) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dragOver(DropTargetDragEvent dtde) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dropActionChanged(DropTargetDragEvent dtde) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dragExit(DropTargetEvent dte) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drop(DropTargetDropEvent dtde) {
+		// TODO Auto-generated method stub
+		try{
+           try {
+                Transferable tr = dtde.getTransferable();
+
+                if (dtde.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                    dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+                    System.out.println("file cp");
+                    List list = (List) (dtde.getTransferable()
+                            .getTransferData(DataFlavor.javaFileListFlavor));
+                    Iterator iterator = list.iterator();
+                    while (iterator.hasNext()) {
+                        File f = (File) iterator.next();
+                        filepath = f.getAbsolutePath();
+    					labfilepath.setText( filepath);
+                        //this.setText(f.getAbsolutePath());
+                    }
+                    dtde.dropComplete(true);
+                    //this.updateUI();
+	            }else {
+	                dtde.rejectDrop();
+	            }
+	        } catch (IOException ioe) {
+	            ioe.printStackTrace();
+	        }
+		}catch(Exception err){
+			err.printStackTrace();
+		}
+	}
+}
---
+++ src/ken/pdfwaterprint/TransparentWatermark.java
@@ -0,0 +1,67 @@
+package ken.pdfwaterprint;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Font.FontFamily;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfGState;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+ 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TransparentWatermark {
+ 
+    public static final String SRC = "resources/pdfs/pages.pdf";
+    public static final String DEST = "results/stamper/pages_watermarked.pdf";
+    public static final String IMG = "./waterprint.png";
+ 
+    /*public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new TransparentWatermark().manipulatePdf(SRC, DEST);
+    }*/
+ 
+    public void manipulatePdf(String src, String dest) throws IOException, DocumentException {
+        PdfReader reader = new PdfReader(src);
+        int n = reader.getNumberOfPages();
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(dest));
+        // text watermark
+        Font f = new Font(FontFamily.HELVETICA, 30);
+        //Phrase p = new Phrase("My watermark (text)", f);
+        // image watermark
+        Image img = Image.getInstance(IMG);
+        float w = img.getScaledWidth();
+        float h = img.getScaledHeight();
+        // transparency
+        PdfGState gs1 = new PdfGState();
+        gs1.setFillOpacity(0.5f);
+        // properties
+        PdfContentByte over;
+        Rectangle pagesize;
+        float x, y;
+        // loop over every page
+        for (int i = 1; i <= n; i++) {
+            pagesize = reader.getPageSizeWithRotation(i);
+            x = (pagesize.getLeft() + pagesize.getRight()) / 2;
+            y = (pagesize.getTop() + pagesize.getBottom()) / 2;
+            over = stamper.getOverContent(i);
+            over.saveState();
+            over.setGState(gs1);
+            //if (i % 2 == 1)
+                //ColumnText.showTextAligned(over, Element.ALIGN_CENTER, p, x, y, 0);
+            //else
+                over.addImage(img, w, 0, 0, h, x - (w / 2), y - (h / 2));
+            over.restoreState();
+        }
+        stamper.close();
+        reader.close();
+    }
+}
